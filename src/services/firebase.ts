6b00bc6408113f79a650f7a677ae84f2b511a7ef
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-10-18 18:19:57 -0300

    Modificando as credenciais do firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -9,18 +9,28 @@ import 'firebase/database';
 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_kEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    // apiKey: "AIzaSyDQi2c63hnObavEEy1yRM33pzFL-RUJpeY",
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_DATABASE_URL,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID
+
+    apiKey: "AIzaSyDQi2c63hnObavEEy1yRM33pzFL-RUJpeY",
+    authDomain: "letmeask-1fb4d.firebaseapp.com",
+    databaseURL: "https://letmeask-1fb4d-default-rtdb.firebaseio.com",
+    projectId: "letmeask-1fb4d",
+    storageBucket: "letmeask-1fb4d.appspot.com",
+    messagingSenderId: "716204357225",
+    appId: "1:716204357225:web:9f7ade5fbb539c13bc94f8"
 };
   
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
   
 
-export const auth = firebase.auth()
-export const database = firebase.database()
+ const auth = firebase.auth()
+ const database = firebase.database()
+
+export {firebase, auth, database}
