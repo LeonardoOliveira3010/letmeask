429c8b48bbe13b8715220fa053be2f90354b4680
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-10-18 18:20:46 -0300

    Ajustando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+
+// Instalando o react-router-dom com typescript: npom i @types/react-router-dom -d
+import {BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Home />
+    // Englobando os componentes no BrowserRouter
+    <BrowserRouter>
+      {/* Exact exibe somente com a rota definida */}
+      <Route path="/" exact={true} component={ Home } /> 
+      <Route path="/room/new" component={ NewRoom } />
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,27 @@
+// import { useHistory } from 'react-router'
+
+import {auth, firebase} from '../services/firebase'
+// import { GoogleAuthProvider } from "firebase/auth";
+
 import ilustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
+import { Button } from '../components/Button'
 
 import '../styles/auth.css'
 
 export function Home (){
+    // Hooks
+    // const history = useHistory()
+
+    // Autenticação com o Google
+    function handleCreateRoom () {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider).then(result => console.log(result))
+
+        // history.push('/room/new')
+    }
+
     return (
         <div id='page-auth'>
             <aside>
@@ -15,17 +32,17 @@ export function Home (){
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button className="create-room" onClick={handleCreateRoom}>
                         <img src={googleIconImg} alt="Logo Google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className="seperator">ou entre em uma sala</div>
                     <form action="">
                         <input 
                             type="text"
                             placeholder="Digite o código da sala"
                         />
-                        <button type='submit'>Entrar na sala</button>
+                        <Button type='submit'>Entrar na sala</Button>
                     </form>
                 </div>
             </main>
